f1600100eb311c9741aecb8febe0bd1b58db170a
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-11-06 12:01:18 +0100

    Added functionality and state to InputRange component

--- src/components/InputRange.js
+++ src/components/InputRange.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import './InputRange.css';
+
 function InputRange(props) {
-    return null;
+    const { min, max, step, title, currState } = props;
+
+    return (
+        <div className="input-range">
+            <span className="input-range__title-indicator">{`${title} ${currState}`}</span>
+            <div className="input-range__input">
+                <span className="input-range__limit">{min}</span>
+                <input type="range" min={min} max={max} step={step}
+                className="input-range__slider" onChange={props.onChange}
+                value={currState} />
+                <span className="input-range__limit">{max}</span>
+            </div>
+        </div>
+    );
 }
 
 export default InputRange;
--- src/components/PasswordGeneratorForm.js
+++ src/components/PasswordGeneratorForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputRange from './InputRange';
 import Checkbox from './Checkbox';
 import Button from './Button';
@@ -6,6 +6,12 @@ import Button from './Button';
 import './PasswordGeneratorForm.css';
 
 function PasswordGeneratorForm(props) {
+    const [passwordLength, setPasswordLength] = useState(12);
+
+    function handlePasswordLength(e) {
+        setPasswordLength(e.target.value);
+    }
+
     return (
         <div className="password-generator">
             <h1 className="password-generator__title">Random Password Generator</h1>
@@ -13,7 +19,8 @@ function PasswordGeneratorForm(props) {
                 <span className="password-generator__password"></span>
                 <Button types={['primary', 'border-primary', 'copy']}>Copy</Button>
             </div>
-            <InputRange />
+            <InputRange min={12} max={36} step={1} title="Password Length:"
+            currState={passwordLength} onChange={handlePasswordLength} />
             <Checkbox />
             <Checkbox />
             <Checkbox />
